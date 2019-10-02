f35d829ae427fb0e8171ac5c6f20c902c41fec45
Olatunbosun Egberinde <bosunski@gmail.com>
2019-10-02 13:46:43 +0100

    Bump dependency Versions to Support Laravel 6

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,7 +21,7 @@ abstract class TestCase extends Orchestra
      *
      * @return void
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
@@ -37,7 +37,7 @@ abstract class TestCase extends Orchestra
      *
      * @return void
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         $this->resetTestingStage();
 
